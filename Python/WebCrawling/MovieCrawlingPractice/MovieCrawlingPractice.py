20147a4c1ec6edc6f20a253a3ad1fd0fddb262bd
donddog <34568203+donddog@users.noreply.github.com>
2019-01-05 02:11:11 +0900

    Ongoing changing codes for getting movie timetable

--- Python/WebCrawling/MovieCrawlingPractice/MovieCrawlingPractice.py
+++ Python/WebCrawling/MovieCrawlingPractice/MovieCrawlingPractice.py
@@ -2,48 +2,63 @@ import requests
 import konlpy as ko
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import re
 
-
-# driver = webdriver.Chrome('/Users/J/Desktop/chromedriver.exe')
-# driver = webdriver.PhantomJS('/Users/J/Desktop/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+# driver = webdriver.Chrome('/Users/LBuser/Desktop/chromedriver.exe')
+# driver = webdriver.PhantomJS('/Users/LBuser/Desktop/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs.exe')
 # driver.implicitly_wait(3)
 # driver.get('/common/showtimes/iframeTheater.aspx?areacode=01&amp;theatercode=0074')
 
-default_cgv = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?'
+# requests = requests.get('http://www.cgv.co.kr/reserve/show-times/?areacode=01&theaterCode=0074&date=20190103')
+# contents = requests.content
+#
+# soup = BeautifulSoup(contents, 'html.parser')
+#
+# #print(soup)
+#
+# body = soup.find("body")
+#
+# print(soup)
+# #li = soup.find
+# # contents = soup.select(
+# #     'li > div'
+# # )
+# # print(soup)
+# # for contentinfo in contents:
+# #     print(contentinfo)
+
+req = requests.get('http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theaterCode=0074&')
 
 #areacode=01&theaterCode=0074&date=20190104 (왕십리)
-area = 'areacode=01&'
-theater = 'theaterCode=0074&'
-date = 'date=20190104'
 
-CGVTime_html = requests.get(default_cgv + area + theater + date).text
-CGVTime_soup = BeautifulSoup(CGVTime_html, 'html.parser')
+html = req.text
+CGV_soup = BeautifulSoup(html, 'html.parser')
 
-movie_info = CGVTime_soup.select(".sect-showtimes .info-movie")
-movie_title = CGVTime_soup.select(".sect-showtimes .info-movie strong")
+movie_time = CGV_soup.select('.sect-showtimes .info-timetable li a')
+movie_title = CGV_soup.select('.sect-showtimes .info-movie strong')
+movie_info = CGV_soup.select('.sect-showtimes .info-hall i li')
+
+print(movie_title)
 
 # 영화 제목
-# for info in movie_info:
-#     print(info.text)
-
-#for title in movie_title:
-#   print(title.text)
-
-movie_time = CGVTime_soup.select(".sect-showtimes .info-timetable li a")
-movie_schedule = CGVTime_soup.select(".sect-schedule .item-wrap span strong")
-
 for title in movie_title:
-    for schedule in movie_schedule:
-        print(title.text + schedule.text)
+    title.text
 
-for time in movie_time:
-    #print(time)
+#titlelist = movie_title.split()
 
-    print("영화제목:", )
-    print("상영시간:", time['data-playstarttime'])
-    print("상영관:", time['data-screenkorname'])
-    print("잔여좌석:", time['data-seatremaincnt'])
-    print("일자:", time['data-playymd'])
+#print(titlelist)
+#titlelist.strip('\n')
+
+#print(titlelist)
+
+for total in movie_time:
+        #print(time)
+
+    print("상영시작시간:", total['data-playstarttime'])
+    print("상영지역:", total['data-theatername'])
+    print("상영관:", total['data-screenkorname'])
+    print("잔여좌석:", total['data-seatremaincnt'])
+    print("일자:", total['data-playymd'])
     print("\n")
 
 # requests = requests.get('http://www.cgv.co.kr/reserve/show-times/?areacode=01&theaterCode=0074&date=20190103')
