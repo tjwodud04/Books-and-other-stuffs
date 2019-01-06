c52a4fb30eb21cfb1f57359f9c114eadea9bfcbb
donddog <34568203+donddog@users.noreply.github.com>
2019-01-07 00:38:09 +0900

    Changing few codes' line and adding tensorflow open source

--- Python/WebCrawling/MovieCrawlingPractice/MovieCrawlingPractice.py
+++ Python/WebCrawling/MovieCrawlingPractice/MovieCrawlingPractice.py
@@ -10,12 +10,20 @@ driver = webdriver.PhantomJS('/Users/J/Desktop/phantomjs-2.1.1-windows/bin/phant
 driver.implicitly_wait(3)
 driver.get('http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theaterCode=0074&')
 
-req = requests.get('http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theaterCode=0074&')
+#'-------------------------------------------------------------------------------------------------------------------------'
 
+req = requests.get('http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0074&date=20190106')
+req2 = requests.get('http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0074&date=20190107')
+req3 = requests.get('http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0074&date=20190108')
 #areacode=01&theaterCode=0074&date=20190104 (왕십리)
 
 html = req.text
+html2 = req2.text
+html3 = req3.text
+
 CGV_soup = BeautifulSoup(html, 'html.parser')
+CGV_soup2 = BeautifulSoup(html2, 'html.parser')
+CGV_soup3 = BeautifulSoup(html3, 'html.parser')
 
 movie_time = CGV_soup.select('.sect-showtimes .info-timetable li a')
 movie_title = CGV_soup.select('.sect-showtimes .info-movie strong')
@@ -23,7 +31,7 @@ movie_info = CGV_soup.select('.sect-showtimes .info-hall i li')
 
 # 영화 제목
 for title in movie_title:
-    title.text
+    print(title.text)
 
 for total in movie_time:
         #print(time)
