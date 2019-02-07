6f0f28de7581fad04e12c9f1c95cd8ce3a0ac31c
donddog <34568203+donddog@users.noreply.github.com>
2019-02-07 21:10:16 +0900

    following some tensorflow tutorials

--- Python/Books&Following/FollowingTensorflowTutorial/TensorflowExample04(textclassification).py
+++ Python/Books&Following/FollowingTensorflowTutorial/TensorflowExample04(textclassification).py
@@ -8,7 +8,27 @@ import tensorflow as tf
 from tensorflow import keras
 
 import numpy as np
-print(tf.__version__)
+print(tf.__version__, "\n")
 
 imdb = keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)), "\n")
+
+print(train_data[0], "\n")
+print(len(train_data[0]), len(train_data[1]), "\n")
+
+word_index = imdb.get_word_index()
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+decode_review(train_data[0])
+
