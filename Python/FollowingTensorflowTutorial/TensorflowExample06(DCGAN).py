2ef61716855b03dd81e1065881ecafbbf4ef8bd7
donddog <34568203+donddog@users.noreply.github.com>
2019-02-18 14:28:02 +0900

    following tensorflow tutorials

---
+++ Python/FollowingTensorflowTutorial/TensorflowExample06(DCGAN).py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import time
+
+from IPython import display
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
