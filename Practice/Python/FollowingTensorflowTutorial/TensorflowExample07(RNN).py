fae4a59e988dbd40d0d79bcd6f8d5a791164ed8b
donddog <34568203+donddog@users.noreply.github.com>
2019-02-24 23:38:26 +0900

    Following tensorflow tutorials

---
+++ Practice/Python/FollowingTensorflowTutorial/TensorflowExample07(RNN).py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import numpy as np
+import os
+import time
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+print('Length of text: {} characters'.format(len(text)), "\n")
