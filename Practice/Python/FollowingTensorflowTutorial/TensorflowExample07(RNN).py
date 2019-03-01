42d72e3aecebb131a55875ea6728a6e4528f6b11
donddog <34568203+donddog@users.noreply.github.com>
2019-03-01 21:55:09 +0900

    following tensorflow tutorials

--- Practice/Python/FollowingTensorflowTutorial/TensorflowExample07(RNN).py
+++ Practice/Python/FollowingTensorflowTutorial/TensorflowExample07(RNN).py
@@ -26,7 +26,11 @@ for char,_ in zip(char2idx, range(20)):
     print(' {:4s}: {:3d},'.format(repr(char), char2idx[char]))
 print(' ...\n}')
 
+print("\n")
+
 print('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
+print("\n")
 
 seq_length = 100
 exmples_per_epoch = len(text)
@@ -36,8 +40,23 @@ char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 for i in char_dataset.take(5):
     print(idx2char[i.numpy()])
 
+print("\n")
+
 sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
 
 for item in sequences.take(5):
     print(repr(''.join(idx2char[item.numpy()])))
+
+print("\n")
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in dataset.take(1):
+    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+    print('Target data: ', repr(''.join(idx2char[target_example.numpy()])))
 
