e3e1966c51b3eea29f0eeedfef69be1af6a89bd2
donddog <34568203+donddog@users.noreply.github.com>
2019-03-31 20:21:00 +0900

    codes for preparing a test

--- Practice/C/정보처리기사용 연습/정보처리기사 실기 C.c
+++ Practice/C/정보처리기사용 연습/정보처리기사 실기 C.c
@@ -284,3 +284,23 @@ int main() {
    int z=x+y;
    printf("Sum of x+y = %i", z);
 }
+
+//021
+#include<stdio.h>
+
+void func(int i, int j);
+main() {
+    int a =3, b = 12;
+    func(a,b);
+    printf("%d %d\n", a, b);
+}
+
+void func(i,j)
+int i, j;
+{
+    i *= 3;
+    j /= 3;
+    printf("%d, %d\n", i,j);
+}
+
+//022
