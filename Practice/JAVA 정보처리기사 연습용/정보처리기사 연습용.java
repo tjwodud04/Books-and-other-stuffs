7b325963a118c3a1801081f62ceaae454d0b5c20
donddog <34568203+donddog@users.noreply.github.com>
2019-04-02 23:48:06 +0900

    following a book and preparing a test

--- Practice/JAVA 정보처리기사 연습용/정보처리기사 연습용.java
+++ Practice/JAVA 정보처리기사 연습용/정보처리기사 연습용.java
@@ -77,3 +77,23 @@ public class Example {
 }
 
 //006
+public class Example {
+    static class AAclass {
+        int i;
+        int j;
+    }
+    public static void main(String[] args) {
+        AAclass myVal = new AAclass();
+        myVal.i = 10;
+        myVal.j = 20;
+        myVal = change(myVal);
+        System.out.printf("i=%d, j =%d\n", myVal.i, myVal.j);
+    }
+    static AAclass change(AAclass myVal) {
+        int temp;
+        temp = myVal.i;
+        myVal.i = myVal.j;
+        myVal.j = temp;
+        return myVal;
+    }
+}
