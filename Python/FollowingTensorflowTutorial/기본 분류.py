82587198260d9e22263dcc166ee753c42342b864
donddog <34568203+donddog@users.noreply.github.com>
2019-10-05 23:40:53 +0900

    sample results and codes of tensorflow&keras for mnist fashion datasets

--- Python/FollowingTensorflowTutorial/기본 분류.py
+++ Python/FollowingTensorflowTutorial/기본 분류.py
@@ -39,3 +39,121 @@ for i in range(25):
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28,28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('테스트 정확도: ', test_acc)
+'''result
+테스트 정확도:  0.8798
+'''
+
+predictions = model.predict(test_images)
+
+print(predictions[0])
+print(np.argmax(predictions[0]))
+print(test_labels[0])
+'''result
+[8.9517334e-06 1.8974816e-07 1.8689404e-07 6.9909440e-08 8.2326528e-07
+ 5.6742071e-03 1.0578525e-05 6.9529049e-02 1.0396940e-04 9.2467201e-01]
+9
+9
+'''
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+plt.show()
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions, test_labels)
+plt.show()
+
+img = test_images[0]
+print(img.shape)
+'''result
+(28, 28)
+'''
+
+img = np.expand_dims(img,0)
+print(img.shape)
+'''result
+(1, 28, 28)
+'''
+
+predictions_single = model.predict(img)
+print(predictions_single)
+'''result
+[8.9517334e-06 1.8974816e-07 1.8689404e-07 6.9909440e-08 8.2326528e-07
+ 5.6742071e-03 1.0578525e-05 6.9529049e-02 1.0396940e-04 9.2467201e-01]
+'''
+
+plot_value_array(0, predictions_single, test_labels)
+plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+prediction_result = np.argmax(predictions_single[0])
+print(prediction_result)
+'''result
+9
+'''
