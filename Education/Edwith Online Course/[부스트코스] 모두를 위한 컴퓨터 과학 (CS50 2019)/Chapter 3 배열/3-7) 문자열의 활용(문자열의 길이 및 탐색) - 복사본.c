a892105bb95b52bddc0759707a252fa8fa9af532
donddog <dddsaty09@naver.com>
2020-07-25 00:10:11 +0900

    Following Online Edwith course CS50 2019 Chapter 3

---
+++ Education/Edwith Online Course/[부스트코스] 모두를 위한 컴퓨터 과학 (CS50 2019)/Chapter 3 배열/3-7) 문자열의 활용(문자열의 길이 및 탐색) - 복사본.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char s[100];
+    printf("Input: ");
+    scanf("%s", s);
+    printf("Output:\n");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c\n", s[i]);
+    }
+}
