1dbeab245271f1ef765ae9db7b00cefeaf231953
donddog <dddsaty09@naver.com>
2020-12-29 01:59:31 +0900

    Following a book '수학과 함께하는 AI 기초' Chapter 3 07

---
+++ 책, 튜토리얼(Following books, tutorials)/Books/수학과 함께하는 AI 기초/3단원/3-2/3-2-05.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+iris = pd.read_csv('Iris.csv')
+
+print(iris.head(2))
+print()
+print(iris.info( ))
+print()
+print(iris.describe( ))
+
+'''
+   Id  SepalLengthCm  SepalWidthCm  PetalLengthCm  PetalWidthCm      Species
+0   1            5.1           3.5            1.4           0.2  Iris-setosa
+1   2            4.9           3.0            1.4           0.2  Iris-setosa
+
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 150 entries, 0 to 149
+Data columns (total 6 columns):
+ #   Column         Non-Null Count  Dtype  
+---  ------         --------------  -----  
+ 0   Id             150 non-null    int64  
+ 1   SepalLengthCm  150 non-null    float64
+ 2   SepalWidthCm   150 non-null    float64
+ 3   PetalLengthCm  150 non-null    float64
+ 4   PetalWidthCm   150 non-null    float64
+ 5   Species        150 non-null    object 
+dtypes: float64(4), int64(1), object(1)
+memory usage: 7.2+ KB
+None
+
+               Id  SepalLengthCm  SepalWidthCm  PetalLengthCm  PetalWidthCm
+count  150.000000     150.000000    150.000000     150.000000    150.000000
+mean    75.500000       5.843333      3.054000       3.758667      1.198667
+std     43.445368       0.828066      0.433594       1.764420      0.763161
+min      1.000000       4.300000      2.000000       1.000000      0.100000
+25%     38.250000       5.100000      2.800000       1.600000      0.300000
+50%     75.500000       5.800000      3.000000       4.350000      1.300000
+75%    112.750000       6.400000      3.300000       5.100000      1.800000
+max    150.000000       7.900000      4.400000       6.900000      2.500000
+'''
