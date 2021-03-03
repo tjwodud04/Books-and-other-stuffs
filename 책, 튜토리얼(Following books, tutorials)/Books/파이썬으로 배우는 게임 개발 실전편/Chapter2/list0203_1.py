a85dcc855eb9a95afb9d08c15d3f4958eb8900ed
donddog <dddsaty09@naver.com>
2021-03-04 00:31:25 +0900

    Following a book '파이썬으로 배우는 게임 개발 실전편' Chapter 2 01

---
+++ 책, 튜토리얼(Following books, tutorials)/Books/파이썬으로 배우는 게임 개발 실전편/Chapter2/list0203_1.py
@@ -0,0 +1,17 @@
+import math
+
+d = 45  # 도
+a = math.radians(d)  # 라디안으로 변환
+s = math.sin(a)
+c = math.cos(a)
+t = math.tan(a)
+
+print("sin " + str(s))
+print("cos " + str(c))
+print("tan " + str(t))
+
+'''
+sin 0.7071067811865476
+cos 0.7071067811865476
+tan 0.9999999999999999
+'''
